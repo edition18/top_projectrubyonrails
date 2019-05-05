c38892acb9ebc2fab82c49300b5e76a1d88cb9cf
Edwin <jchoo004@e.ntu.sg>
2019-05-05 18:26:08 +0700

    night

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -10,5 +10,18 @@ class ArticlesController < ApplicationController
         # Within that hash we can find the :id from the URL by accessing the key params[:id]. 
         # Use this inside the show method of ArticlesController along with the class method find on the Article class:
     end
+
+    def new
+        @article = Article.new
+
+    end
+
+    def create
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.save
+        redirect_to article_path(@article)
+    end
+    
     
 end
