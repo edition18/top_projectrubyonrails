328efad71dccb8627cd27fe816eaa0bd11566c2a
Edwin <jchoo004@e.ntu.sg>
2019-07-28 07:59:25 +0100

    28 jul

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -10,6 +10,8 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         # Within that hash we can find the :id from the URL by accessing the key params[:id]. 
         # Use this inside the show method of ArticlesController along with the class method find on the Article class:
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
 
     def new
